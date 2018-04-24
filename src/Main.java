91a65fbdc7f2d121900a564a7bee070f96b370a9
Alex Studer <astuder@notetoscreen.com>
2018-04-23 21:35:54 -0400

    add remaining edges

--- src/Main.java
+++ src/Main.java
@@ -142,5 +142,93 @@ public class Main {
 		board.addPath("St Louis", "Nashville", 2, 1);
 		board.addPath("St Louis", "Pittsburgh", 5, 1);
 
+		board.addPath("Chicago", "St Louis", 2, 2);
+		board.addPath("Chicago", "Omaha", 4, 1);
+		board.addPath("Chicago", "Duluth", 3, 1);
+		board.addPath("Chicago", "Toronto", 4, 1);
+		board.addPath("Chicago", "Pittsburgh", 4, 1);
+
+		board.addPath("Duluth", "Helena", 6, 1);
+		board.addPath("Duluth", "Winnipeg", 4, 1);
+		board.addPath("Duluth", "Omaha", 2, 2);
+		board.addPath("Duluth", "Sault St Marie", 3, 1);
+		board.addPath("Duluth", "Toronto", 6, 1);
+		board.addPath("Duluth", "Chicago", 3, 1);
+
+		board.addPath("Omaha", "Helena", 5, 1);
+		board.addPath("Omaha", "Duluth", 2, 2);
+		board.addPath("Omaha", "Denver", 4, 1);
+		board.addPath("Omaha", "Kansas City", 2, 2);
+		board.addPath("Omaha", "Chicago", 4, 1);
+
+		board.addPath("New Orleans", "Houston", 2, 1);
+		board.addPath("New Orleans", "Little Rock", 3, 1);
+		board.addPath("New Orleans", "Atlanta", 4, 2);
+		board.addPath("New Orleans", "Miami", 6, 1);
+
+		board.addPath("Sault St Marie", "Winnipeg", 6, 1);
+		board.addPath("Sault St Marie", "Duluth", 3, 1);
+		board.addPath("Sault St Marie", "Montreal", 5, 1);
+		board.addPath("Sault St Marie", "Toronto", 2, 1);
+
+		board.addPath("Winnipeg", "Sault St Marie", 6, 1);
+		board.addPath("Winnipeg", "Calgary", 6, 1);
+		board.addPath("Winnipeg", "Helena", 4, 1);
+		board.addPath("Winnipeg", "Duluth", 4, 1);
+
+		board.addPath("Montreal", "Sault St Marie", 5, 1);
+		board.addPath("Montreal", "Toronto", 3, 1);
+		board.addPath("Montreal", "New York", 3, 1);
+		board.addPath("Montreal", "Boston", 2, 2);
+
+		board.addPath("Toronto", "Sault St Marie", 2, 1);
+		board.addPath("Toronto", "Duluth", 6, 1);
+		board.addPath("Toronto", "Pittsburgh", 2, 1);
+		board.addPath("Toronto", "Montreal", 3, 1);
+		board.addPath("Toronto", "Chicago", 4, 1);
+
+		board.addPath("Boston", "Montreal", 2, 2);
+		board.addPath("Boston", "New York", 2, 2);
+
+		board.addPath("New York", "Boston", 2, 2);
+		board.addPath("New York", "Pittsburgh", 2, 2);
+		board.addPath("New York", "Washington", 2, 2);
+
+		board.addPath("Pittsburgh", "Toronto", 2, 1);
+		board.addPath("Pittsburgh", "Chicago", 3, 2);
+		board.addPath("Pittsburgh", "St Louis", 5, 1);
+		board.addPath("Pittsburgh", "Nashville", 4, 1);
+		board.addPath("Pittsburgh", "Raleigh", 2, 1);
+		board.addPath("Pittsburgh", "Washington", 2, 1);
+		board.addPath("Pittsburgh", "New York", 2, 2);
+
+		board.addPath("Washington", "New York", 2, 2);
+		board.addPath("Washington", "Pittsburgh", 2, 1);
+		board.addPath("Washington", "Raleigh", 2, 2);
+
+		board.addPath("Nashville", "St Louis", 2, 1);
+		board.addPath("Nashville", "Pittsburgh", 4, 1);
+		board.addPath("Nashville", "Raleigh", 3, 1);
+		board.addPath("Nashville", "Little Rock", 3, 1);
+		board.addPath("Nashville", "Atlanta", 1, 1);
+
+		board.addPath("Raleigh", "Washington", 2, 2);
+		board.addPath("Raleigh", "Nashville", 3, 1);
+		board.addPath("Raleigh", "Atlanta", 2, 2);
+		board.addPath("Raleigh", "Charleston", 2, 1);
+
+		board.addPath("Atlanta", "Nashville", 1, 1);
+		board.addPath("Atlanta", "Raleigh", 2, 2);
+		board.addPath("Atlanta", "Charleston", 2, 1);
+		board.addPath("Atlanta", "New Orleans", 4, 2);
+		board.addPath("Atlanta", "Miama", 5, 1);
+
+		board.addPath("Charleston", "Raleigh", 2, 1);
+		board.addPath("Charleston", "Atlanta", 2, 1);
+		board.addPath("Charleston", "Miami", 4, 1);
+
+		board.addPath("Miami", "New Orleans", 5, 1);
+		board.addPath("Miami", "Atlanta", 5, 1);
+		board.addPath("Miami", "Charleston", 4, 1);
 	}
 }
