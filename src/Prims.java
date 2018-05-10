88b0f1e6b03f1a25eaf30f7b3550eb8a47f0c1fd
acohen <c19ac@daltong.org>
2018-05-10 14:33:56 -0400

    pls help

--- src/Prims.java
+++ src/Prims.java
@@ -2,41 +2,28 @@ import java.util.*;
 public class Prims {
     public static Tree Prims(Graph input) {
         boolean done = false;
-        int CurrentCitiesIndex = 0;
         Boolean checkedFullTree= false;
         Edge smallestLengthEdge = null;
         Tree MST = new Tree();
-        int[] currentcities = new int [input.adjacency.length+100];
+        ArrayList<Integer> currentcities = new ArrayList<>();
         int counter = 0;
         print(input);
-        currentcities[0]=0;
         while (!done) {
             int finito = 0;
             checkedFullTree= false;
             while(!checkedFullTree) {
-                smallestLengthEdge = getStartingLength(input);
-                for (int x = 0; x < currentcities.length-1; x++){
-                    for (int y = 0; y < input.adjacency.length - 2; y++) {
-                        if (input.adjacency[currentcities[x]][y] != 0) {
-//                    System.out.println(input.places.get(x) + " --> " + input.places.get(y) + " (" + input.adjacency[x][y] + ")");
-                            if (input.adjacency[currentcities[x]][y] <= smallestLengthEdge.weight && !MST.nodes.contains(input.places.get(y))) {
-//                                System.out.println(input.places.get(currentcities[x]) + " --> " + input.places.get(y) + " (" + input.adjacency[currentcities[x]][y] + ")");
-//                                System.out.println(input.places.get(y));
-                                smallestLengthEdge = new Edge(input.places.get(x), input.places.get(y), input.adjacency[x][y]);
-                                currentcities[CurrentCitiesIndex]=y;
-//                                System.out.println("smallest edge" + smallestLengthEdge);
-                            }
+                for (int x = 0; x < input.adjacency.length-1; x++){
+                    boolean added = false;
+                   // while (!added) {
+                        smallestLengthEdge = getStartingLength(input, x);
+                        if (!MST.addingEdgeWouldCauseCycle(smallestLengthEdge)) {
+                            MST.addEdge(smallestLengthEdge);
+                            System.out.println("adding");
+                            added = true;
                         }
-                    }
-                    System.out.println("next tree in the forest");
+                   // }
                 }
-                System.out.println("checked the whole forest");
                 checkedFullTree=true;
-            }
-            if(!MST.addingEdgeWouldCauseCycle(smallestLengthEdge)) {
-                MST.addEdge(smallestLengthEdge);
-                System.out.println("adding");
-                CurrentCitiesIndex++;
             }
 
             for (int i = 0; i < input.adjacency.length-2; i++) {
@@ -47,8 +34,9 @@ public class Prims {
             counter++;
             if (counter > input.places.size()) done = true;
         }
-            System.out.println("done");
-            return MST;
+
+        System.out.println("done");
+        return MST;
     }
 
 
@@ -61,11 +49,10 @@ public class Prims {
         }
     }
 
-    public static Edge getStartingLength(Graph input) {
-        int x = 0;
+    public static Edge getStartingLength(Graph input, int x) {
         Edge smallestLengthEdge = null;
         for (int z = 0; z < input.adjacency.length - 2; z++) {
-            if (input.adjacency[x][z] != 0) {
+            if (input.adjacency[x][z] != 0 && (smallestLengthEdge == null || input.adjacency[x][z] < smallestLengthEdge.weight)) {
                 smallestLengthEdge = new Edge(input.places.get(x), input.places.get(z), input.adjacency[x][z]);
             }
         }
