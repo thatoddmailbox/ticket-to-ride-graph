8b0a9ea06c1c897ab71ac241f8cdd89d585f8498
Emlyn <c19em@dalton.org>
2018-05-21 10:16:36 -0400

    moby dick

--- src/bestestShortest_path.java
+++ src/bestestShortest_path.java
@@ -2,7 +2,8 @@ import java.util.HashMap;
 
 public class bestestShortest_path {
     public static HashMap<String, Integer> findShortestPathsToOtherPlacesYeah_WOW(Graph g, String wya){
-return null;
+        //HashMap<String, Integer>;
+        return null;
     }
 }
 
